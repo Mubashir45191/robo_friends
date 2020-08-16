38dfcd988f67a9c52d4f5c6050ffc45489d6c66a
Mubashir45191 <mubashirhasan716@gmail.com>
2020-08-16 18:55:54 +0530

    finished

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,9 @@
 import React from "react";
-import CardList from "./CardList";
-import SearchBox from "./SearchBox";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
 // import { robots } from "./robots";
 import "./App.css";
+import Scroll from '../components/Scroll';
 
 class App extends React.Component {
   constructor() {
@@ -36,19 +37,21 @@ class App extends React.Component {
                  .toLowerCase()
                  .includes(this.state.searchField.toLowerCase());
              });
-             if (this.state.robots.length === 0) {
-               return <h1>Loading...</h1>;
-             } else {
+             return !this.state.robots.length ?
+                <h1>Loading...</h1>:
+        
                // console.log('render');
 
-               return (
+                (
                  <div className="tc">
                    <h1 className="f1">Robo Friends</h1>
                    <SearchBox searchChange={this.onSearchChange} />
-                   <CardList robots={filteredRobots} />
+                   <Scroll>
+                     <CardList robots={filteredRobots} />
+                   </Scroll>
                  </div>
                );
-             }
+            
            }
 }
 
